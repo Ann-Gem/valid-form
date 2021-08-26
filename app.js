c94ee28ad927771dac48c6c38dc5c2cf4a84dff5
Ann-Gem <anngem0101@gmail.com>
2021-08-26 23:05:51 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const inputs = document.querySelectorAll('input');
+
+const patterns = {
+    name: /^[a-zA-Z]{3,10}$/,
+    email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/,
+    // password: /^[\w@-\d]{8,20}$/,
+    password: /^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z]).{8,}$/,
+    phone: /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,11}$/,
+    url: /^https?:\/\/[\w\/?.&-=]+$/
+}
+
+function validate(field, regExp) {
+    if(regExp.test(field.value)) {
+        field.className = 'valid';
+    } else {
+        field.className = 'invalid';
+    }
+}
+inputs.forEach((input) => {
+    input.addEventListener('keyup', (e) => {
+        validate(e.target, patterns[e.target.attributes.name.value]);
+    })
+})
+
+
+
+
+
+
